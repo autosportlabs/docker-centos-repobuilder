20b0c595c78e9875401fbb32c0f4d0e4b23d87ec
Brian Lalor <brian@autosportlabs.com>
2014-01-06 23:16:54 -0500

    Verify presence of environment vars, point to asl local repo

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+[ -z $AWS_ACCESS_KEY ] && { echo "no value for AWS_ACCESS_KEY"; exit 1; }
+[ -z $AWS_SECRET_KEY ] && { echo "no value for AWS_SECRET_KEY"; exit 1; }
+[ -z $BUCKET ]         && { echo "no value for BUCKET"; exit 1; }
+[ -z $REPO ]           && { echo "no value for REPO"; exit 1; }
+
+set -e
+
 basedir=$( cd $( dirname $0 ) && /bin/pwd )
 
 [ -d ${basedir}/rpms ] || mkdir -p ${basedir}/rpms
@@ -11,5 +18,5 @@ docker run \
     -e AWS_SECRET_KEY \
     -e BUCKET \
     -e REPO \
-    blalor/centos-buildtools:latest \
+    localhost:5000/tools/centos-buildtools \
     /scripts/build_all.sh
